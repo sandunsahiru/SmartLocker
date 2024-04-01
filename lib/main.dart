79e1b793ad823f12c7607faf538b0347670862cb
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2024-04-01 21:29:20 +0530

    Firebase Connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:smartlocker/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:smartlocker/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
