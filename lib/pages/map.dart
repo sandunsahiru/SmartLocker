4a1cefc23de5e0a8d75b4a1036b8520a45489221
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2024-04-08 23:29:06 +0530

    Last Working Code

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -10,42 +10,38 @@ class MapPage extends StatefulWidget {
   const MapPage({Key? key, required this.deviceId}) : super(key: key);
 
   @override
-  _MapPageState createState() => _MapPageState();
+  State<MapPage> createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
   late final FirebaseDatabase database;
-  GoogleMapController? mapController;
-  LatLng _currentPosition = LatLng(6.821934686130015, 80.04154817409899);
+  GoogleMapController? _googleMapController;
+  LatLng _currentPosition = LatLng(6.821934686130015, 80.04154817409899); // Initial position
   bool _isLoading = true;
   String _errorMessage = '';
 
   @override
   void initState() {
     super.initState();
-    // Initialize Firebase Database with the specified URL.
     database = FirebaseDatabase(databaseURL: "https://smartlockerintellilock-default-rtdb.asia-southeast1.firebasedatabase.app");
-    print('Initializing MapPage with deviceId: ${widget.deviceId}'); // Print the device ID for debugging
-    _listenToLocationChanges();
+    _fetchLocation();
   }
 
-  void _listenToLocationChanges() {
-    print('Setting up location listener...'); // Indicates listener setup started
+  void _fetchLocation() {
     DatabaseReference ref = database.ref('${widget.deviceId}/location');
     ref.onValue.listen((DatabaseEvent event) {
-      print('Location data stream received: ${event.snapshot.value}'); // Indicates data received
       final data = event.snapshot.value as Map<dynamic, dynamic>?;
       if (data != null) {
         final latitude = data['latitude'] as double?;
         final longitude = data['longitude'] as double?;
-        print('Fetched Latitude: $latitude, Fetched Longitude: $longitude'); // Print the fetched coordinates
-
         if (latitude != null && longitude != null) {
           setState(() {
             _currentPosition = LatLng(latitude, longitude);
             _isLoading = false;
           });
-          _updateMapLocation(_currentPosition);
+          _googleMapController?.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(target: _currentPosition, zoom: 15),
+          ));
         } else {
           setState(() {
             _errorMessage = 'Invalid location data';
@@ -59,7 +55,6 @@ class _MapPageState extends State<MapPage> {
         });
       }
     }, onError: (error) {
-      print('Error fetching location data: $error'); // Indicates an error occurred
       setState(() {
         _errorMessage = error.toString();
         _isLoading = false;
@@ -67,26 +62,21 @@ class _MapPageState extends State<MapPage> {
     });
   }
 
-  void _updateMapLocation(LatLng position) {
-    if (mapController != null) {
-      mapController!.animateCamera(CameraUpdate.newLatLng(position));
-    } else {
-      print('Map controller not yet initialized');
-    }
+  @override
+  void dispose() {
+    _googleMapController?.dispose();
+    super.dispose();
   }
 
   void _onMapCreated(GoogleMapController controller) {
-    setState(() {
-      mapController = controller;
-    });
+    _googleMapController = controller;
     if (!_isLoading) {
-      _updateMapLocation(_currentPosition);
+      controller.animateCamera(CameraUpdate.newLatLng(_currentPosition));
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    print('Building MapPage...'); // Indicates the build method is running
     return Scaffold(
       appBar: AppBar(
         title: Text('Device Location'),
@@ -96,23 +86,26 @@ class _MapPageState extends State<MapPage> {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : _errorMessage.isNotEmpty
-          ? Center(child: Text(_errorMessage))
-          : GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _currentPosition,
-          zoom: 10.0,
+      body: SizedBox(
+        height: 300, // Specify the height you want
+        width: double.infinity, // Use double.infinity to match the parent's width
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : _errorMessage.isNotEmpty
+            ? Center(child: Text(_errorMessage))
+            : GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(target: _currentPosition, zoom: 15),
+          markers: {
+            if (_currentPosition != null)
+              Marker(
+                markerId: MarkerId('deviceLocation'),
+                position: _currentPosition,
+              ),
+          },
         ),
-        markers: Set.from([
-          Marker(
-            markerId: MarkerId("deviceLocation"),
-            position: _currentPosition,
-          ),
-        ]),
       ),
+
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
@@ -121,10 +114,13 @@ class _MapPageState extends State<MapPage> {
         currentIndex: 0,
         selectedItemColor: Theme.of(context).colorScheme.secondary,
         onTap: (index) {
-          if (index == 0) {
-            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => HomePage()));
-          } else if (index == 1) {
-            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => SettingsPage()));
+          switch (index) {
+            case 0:
+              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => HomePage()));
+              break;
+            case 1:
+              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => SettingsPage()));
+              break;
           }
         },
       ),
